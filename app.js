0910570e72b7a92e9140cba1ad7a6245f80b5668
Erwan31 <spilmonterwan@gmail.com>
2020-06-05 18:11:55 +0100

    Bubble animation onClick burger

---
+++ app.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('nav-links li');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle("open");
+})
