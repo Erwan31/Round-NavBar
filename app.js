f2b2f095e50fc9364d1804f4f2e021b585cbe53e
Erwan31 <spilmonterwan@gmail.com>
2020-06-05 18:21:04 +0100

    Responsive Animation done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const burger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('nav-links li');
+const links = document.querySelectorAll('.nav-links li');
 
 burger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
+    links.forEach( link => {
+        link.classList.toggle("fade");
+    })
 })
