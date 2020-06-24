961d1d6be46625ba934dd371da241b88a9cb8072
Erwan31 <spilmonterwan@gmail.com>
2020-06-24 10:31:59 +0100

    Before After burger

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const burger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const line = document.querySelector('.line');
 
 burger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
     links.forEach( link => {
         link.classList.toggle("fade");
     })
+
+    line.classList.toggle('open');
+
 })
